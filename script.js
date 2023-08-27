9b9326a9ee64710d16220e590132c9f46e9fb09d
galrothschild <131006606+galrothschild@users.noreply.github.com>
2023-08-27 06:59:26 +0300

    adding some functionality to the stopwatch

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const scrambleOptions = ["F", "B", "U", "D", "R", "L"];
 const scrambleAddonOptions = ["2", "'", ""];
+let spacePressed = false;
+let stopwatchInitiated = false;
+let stopwatchRunning = false;
+let stopwatchID = 0;
+function init() {
+    display(generateScamble().join(" "));
+    setUserControls();
+}
+init();
 
 function randomIntInRange(min = 0, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -35,9 +44,6 @@ function reverseScramble(scramble) {
     });
     return reverse;
 }
-let scramble = generateScamble();
-console.log(scramble.join(" "));
-console.log(reverseScramble(scramble).join(" "));
 function display(scramble) {
     let scrambleElement = document.createElement("scramble-display");
     let scrambleAlgorithm = document.createElement("p");
@@ -48,4 +54,66 @@ function display(scramble) {
     header.appendChild(scrambleElement);
     header.appendChild(scrambleAlgorithm);
 }
-display(scramble.join(" "));
+
+function setUserControls() {
+
+    window.addEventListener("keydown", async (event) => {
+        console.log(spacePressed);
+        if (event.key === " " && !spacePressed) {
+            spacePressed = true;
+            document.getElementById("seconds").innerText = "00";
+            document.getElementById("hudredthSecond").innerText = "00";
+            await startStopwatch();
+        } else if (event.key === " " && stopwatchInitiated && stopwatchRunning) {
+            clearInterval(stopwatchID);
+            stopwatchRunning = false;
+            stopwatchInitiated = false;
+        }
+    });
+    window.addEventListener("keyup", (event) => {
+        console.log(stopwatchInitiated, stopwatchRunning);
+        if (event.key === " " && !stopwatchInitiated) {
+            spacePressed = false;
+        } else if (event.key === " " && stopwatchInitiated && !stopwatchRunning) {
+            time.style.color = "";
+            runStopwatch();
+        }
+    });
+
+}
+function startStopwatch() {
+    return new Promise((resolve, reject) => {
+        let time = document.getElementById("time");
+        time.style.color = "red";
+        let id = setTimeout(() => {
+            time.style.color = "green";
+            stopwatchInitiated = true;
+            console.log(stopwatchInitiated);
+            resolve();
+        }, 1000);
+
+        window.addEventListener("keyup", (event) => {
+            if (event.key === " " && !stopwatchInitiated) {
+                clearTimeout(id);
+                time.style.color = "";
+            }
+        });
+    });
+}
+function runStopwatch() {
+    console.log("object");
+    let secondElement = document.getElementById("seconds");
+    let hundredthSecondElement = document.getElementById("hudredthSecond");
+    let seconds = 0;
+    let hundredthSecond = 0;
+    stopwatchRunning = true;
+    stopwatchID = setInterval(() => {
+        hundredthSecondElement.innerText = `${hundredthSecond}`.padStart(2, "0");
+        secondElement.innerText = `${seconds}`.padStart(2, "0");
+        hundredthSecond++;
+        if (hundredthSecond === 100) {
+            seconds++;
+            hundredthSecond = 0;
+        }
+    }, 10);
+}
